e7904a7977068e1a6d81f83c180f50dcad504b2f
Yunfei Jing <yunfei.jing@outlook.com>
2022-05-12 15:33:31 +0800

    first commit with post entity

---
+++ src/main/java/com/swen/blog/SwenBlogApplication.java
@@ -0,0 +1,13 @@
+package com.swen.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwenBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwenBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swen/blog/SwenBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swen.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwenBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
